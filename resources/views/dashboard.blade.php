797c9fb6037d7983026c284ebc322e129eea411c
Frankllin151 <frankllinsilva300@gmail.com>
2025-06-14 16:02:51 -0300

    first-commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,23 @@
+<x-app-layout>
+    <!--IMPORTANTE NAO REMOVA O x-slot no front-end não vai aparece
+    o componente navigation
+    -->
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+           
+        </h2>
+    </x-slot>
+<div class="d-flex flex-wrap align-items-center justify-content-between gap-3 mb-24">
+  <h6 class="fw-semibold mb-0"> {{ __('Dashboard') }}</h6>
+  <ul class="d-flex align-items-center gap-2">
+    <li class="fw-medium">
+      <a href="index.html" class="d-flex align-items-center gap-1 hover-text-primary">
+        <iconify-icon icon="solar:home-smile-angle-outline" class="icon text-lg"></iconify-icon>
+    {{ __('Dashboard') }}
+      </a>
+    </li>
+   
+    
+  </ul>
+</div>
+</x-app-layout>
